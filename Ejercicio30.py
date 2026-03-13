3f0893459a9057e2276bc8ff01801e68ee0a48a3
Sebastian Sierra <sebastiansierra3112@gmail.com>
2026-03-13 13:39:39 +0100

    Add files via upload

--- Ejercicio30.py
+++ Ejercicio30.py
@@ -1,7 +1,6 @@
 frase = input("Ingresá una frase: ")
 caracter = input("Ingresá un carácter: ")
 
-# Nos aseguramos de tomar solo el primer carácter por las dudas
 caracter = caracter[0]
 
 frase_reemplazada = frase.replace(caracter, "*")
